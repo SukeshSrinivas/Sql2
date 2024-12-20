d8b7c026ecdd34f59c8ba9abe92ef92cb9bcd734
SukeshSrinivas <123338848+SukeshSrinivas@users.noreply.github.com>
2024-12-19 18:39:02 -0600

    Completed Problem

--- Problem2_ExchangeSeats.sql
+++ Problem2_ExchangeSeats.sql
@@ -10,3 +10,14 @@ WHEN CTE.oddcheck='even' THEN
  LAG(CTE.student,1,0) OVER (Order by id) 
  ELSE CTE.student END AS student
  FROM CTE
+
+/*
+Alternative Learned from Web
+SELECT 
+    id,
+    CASE
+        WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
+    END AS student
+FROM Seat
+*/
